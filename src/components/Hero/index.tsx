3261aa2d88439163c6b8c303921f0797a260cd61
srivastavaman <amansrivastava3516@gmail.com>
2024-05-11 23:47:53 +0530

    first commit

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -15,8 +15,7 @@ const Hero = () => {
                 Welcome to Learnalaya
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                Where learning meets Opportunity! Dive into our diverse courses and resources designed for learners of all ages and backgrounds. Whether you're seeking new skills or satisfying your curiosity, embark on your journey with us today
-                </p>
+             {`   Where learning meets Opportunity! Dive into our diverse courses and resources designed for learners of all ages and backgrounds. Whether you're seeking new skills or satisfying your curiosity, embark on your journey with us today `}  </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="./"
